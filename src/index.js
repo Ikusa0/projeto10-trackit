01a38bebd25f9d5aaecf378689335d3cd6d35ffd
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-05-23 23:47:09 -0300

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./Assets/Styles/reset.css";
+import "./Assets/Styles/style.css";
+
+ReactDOM.render(<App />, document.querySelector(".root"));
